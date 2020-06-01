fce669aa3acec3a9beec3baafa42c5064e353e73
xdrie <xdrie@alt.icu>
2020-05-31 22:12:46 -0700

    add context as participants

--- src/DuckMind/Ducia/Cogs/Social/Interaction.cs
+++ src/DuckMind/Ducia/Cogs/Social/Interaction.cs
@@ -1,9 +1,9 @@
 namespace Ducia.Cogs.Social {
-    public abstract class Interaction<TSentient, TPersonality, TTraits, TEmotions>
+    public abstract class Interaction<TContext, TSentient, TPersonality, TTraits, TEmotions>
         where TPersonality : Personality, new()
         where TTraits : Traits<TPersonality>, new()
         where TEmotions : Emotions, new()
         where TSentient : Sentient<TPersonality, TTraits, TEmotions> {
-        public abstract void run(params TSentient[] participants);
+        public abstract void run(params TContext[] participants);
     }
 }
