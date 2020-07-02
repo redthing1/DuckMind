1860c213615b0e1d82052c065f2de9543c915fe0
xdrie <xdrie@alt.icu>
2020-07-02 16:47:51 -0700

    ant sample

--- src/DuckMind/MindDemos/AntMind.cs
+++ src/DuckMind/MindDemos/AntMind.cs
@@ -1,9 +1,19 @@
+using System.Threading;
 using Ducia;
 
 namespace MindDemos {
     public class AntMind : Mind<AntMind.State> {
         public class State : MindState { }
 
-        public AntMind(State state) : base(state) { }
+        public class PheromoneSystem : MindSystem<AntMind, State> {
+            public PheromoneSystem(AntMind mind, float refresh, CancellationToken cancelToken) : base(mind, refresh, cancelToken) { }
+            protected override void process() {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public AntMind(State state) : base(state) {
+            sensorySystems.Add(new PheromoneSystem(this, 0.2f, cancelToken.Token));
+        }
     }
 }
