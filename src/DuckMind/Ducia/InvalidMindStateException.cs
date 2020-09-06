46133b9f742f3944862935993e6db810c508cd2f
xdrie <xdrie@alt.icu>
2020-09-06 16:07:31 -0700

    codedoc and mind state asserts

---
+++ src/DuckMind/Ducia/InvalidMindStateException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Ducia {
+    /// <summary>
+    /// indicates that the mind is in an invalid state
+    /// </summary>
+    public class InvalidMindStateException : Exception {
+        public InvalidMindStateException(string message) : base(message) { }
+    }
+}
--- src/DuckMind/Ducia/Layers/Mind/Systems/PlannerSystem.cs
+++ src/DuckMind/Ducia/Layers/Mind/Systems/PlannerSystem.cs
@@ -6,7 +6,11 @@ namespace Ducia.Systems {
         protected PlannerSystem(TMind mind, float refresh, CancellationToken cancelToken) : base(mind, refresh,
             cancelToken) { }
 
-        protected int maxSignalsPerThink = 40;
+        /// <summary>
+        /// sets the limit on the maximum number of signals that can be processed per update.
+        /// set to 0 to disable limit.
+        /// </summary>
+        protected int maxSignalsPerUpdate = 0;
 
         protected override void process() {
             // look at signals
@@ -24,17 +28,40 @@ namespace Ducia.Systems {
             while (state.signalQueue.TryPeek(out var signal)) {
                 // process the signal
                 var result = processSignal(signal);
+                // if successfully handled, remove it from the queue
                 if (result) {
                     state.signalQueue.TryDequeue(out _);
                 }
+                else {
+                    // signal failed to be handled
+                    // ???
+                    throw new InvalidMindStateException("signal failed to be processed");
+                }
 
                 processedSignals++;
-                if (processedSignals > maxSignalsPerThink) break;
+
+                // if we hit the signal limit, stop
+                if (maxSignalsPerUpdate > 0)
+                    if (processedSignals > maxSignalsPerUpdate)
+                        break;
             }
         }
 
+        /// <summary>
+        /// handle mind signals
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns>whether the signal was handled</returns>
         protected abstract bool processSignal(MindSignal result);
+
+        /// <summary>
+        /// preprocessing to run on results of sensory systems
+        /// </summary>
         protected abstract void processSenses();
+
+        /// <summary>
+        /// make plans based on available information
+        /// </summary>
         protected abstract void makePlans();
     }
 }
