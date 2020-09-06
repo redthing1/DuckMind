5cbc6a577f411eb2589414ad8c20b5fb1121b1ca
xdrie <xdrie@alt.icu>
2020-09-06 15:31:20 -0700

    add codedoc for MindState

--- src/DuckMind/Ducia/Layers/Mind/MindState.cs
+++ src/DuckMind/Ducia/Layers/Mind/MindState.cs
@@ -1,12 +1,21 @@
 using System.Collections.Concurrent;
 
 namespace Ducia {
+    /// <summary>
+    /// represents all data that is considered "state" or "working" information of the mind.
+    /// it can include things like caches for senses and observations
+    /// and plans and working memory
+    /// </summary>
     public abstract class MindState {
-        public int ticks = 0;
+        protected int ticks = 0;
         public float totalTime = 0f;
         
         public ConcurrentQueue<MindSignal> signalQueue = new ConcurrentQueue<MindSignal>(); // signals to be processed
         
+        /// <summary>
+        /// called by the Mind, representing elapsed time
+        /// </summary>
+        /// <param name="elapsed"></param>
         public virtual void tick(float elapsed) {
             ticks++;
             totalTime += elapsed;
