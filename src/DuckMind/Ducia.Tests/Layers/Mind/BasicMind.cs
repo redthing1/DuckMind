778e93337b3058d9799cc643718dd27f78f7982c
xdrie <xdrie@alt.icu>
2020-09-06 19:14:36 -0700

    add some basic mind and system tests

--- src/DuckMind/Ducia.Tests/Layers/Mind/BasicMind.cs
+++ src/DuckMind/Ducia.Tests/Layers/Mind/BasicMind.cs
@@ -1,7 +1,47 @@
+using System;
+using System.Threading;
+
 namespace Ducia.Tests.Layers.Mind {
     public class BasicMind : Mind<BasicMind.State> {
         public class State : MindState { }
 
         public BasicMind() : base(new State()) { }
+
+        public override void initialize() {
+            base.initialize();
+
+            sensorySystems.Add(new RhythmSystem(this, 0.5f, cancelToken.Token));
+            cognitiveSystems.Add(new RandomSeedSystem(this, 0.5f, cancelToken.Token));
+        }
+
+        /// <summary>
+        /// represents a system that tracks beats. every time it processes, it simply increments the beat counter.
+        /// </summary>
+        public class RhythmSystem : MindSystem<BasicMind, State> {
+            public int beats = 0;
+
+            public RhythmSystem(BasicMind mind, float refresh, CancellationToken cancelToken) : base(mind, refresh,
+                cancelToken) { }
+
+            protected override void process() {
+                beats++;
+            }
+        }
+
+        /// <summary>
+        /// represents a system that calculates and stores a random number
+        /// </summary>
+        public class RandomSeedSystem : MindSystem<BasicMind, State> {
+            public Random rng = new Random();
+            public int state = -1; // -1 indicates uninitialized
+
+            public RandomSeedSystem(BasicMind mind, float refresh, CancellationToken cancelToken) : base(mind, refresh,
+                cancelToken) { }
+
+            protected override void process() {
+                // this always gives us a nonnegative integer
+                state = rng.Next();
+            }
+        }
     }
 }
--- src/DuckMind/Ducia.Tests/Layers/Mind/MindTests.cs
+++ src/DuckMind/Ducia.Tests/Layers/Mind/MindTests.cs
@@ -1,17 +1,38 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Ducia.Tests.Layers.Mind {
-    public class MindTests {
+    public class MindTests : IDisposable {
         private BasicMind mind;
 
         public MindTests() {
             mind = new BasicMind();
+            mind.initialize();
         }
 
         [Fact]
         public void canConstructMind() {
             Assert.NotNull(mind.state);
+        }
+
+        [Fact]
+        public void canAddSystems() {
+            Assert.NotEmpty(mind.sensorySystems);
+            Assert.NotEmpty(mind.cognitiveSystems);
+        }
+
+        [Fact]
+        public void canUpdateSystems() {
+            mind.tick(0.1f);
+            var rhythmSystem = (BasicMind.RhythmSystem) mind.sensorySystems.Single(x => x is BasicMind.RhythmSystem);
+            Assert.True(rhythmSystem.beats > 0);
+            var rngSystem = (BasicMind.RandomSeedSystem) mind.cognitiveSystems.Single(x => x is BasicMind.RandomSeedSystem);
+            Assert.True(rngSystem.state >= 0);
+        }
+
+        public void Dispose() {
+            mind.destroy();
         }
     }
 }
