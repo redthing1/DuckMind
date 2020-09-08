ebfb3f079b114eb1ee57116d144f63857777ca1a
xdrie <xdrie@alt.icu>
2020-09-07 18:04:17 -0700

    remove Traits vec

--- src/DuckMind/Ducia/Cogs/Traits.cs
+++ src/DuckMind/Ducia/Cogs/Traits.cs
@@ -1,7 +1,5 @@
 namespace Ducia.Cogs {
     public abstract class Traits<TPersonality> where TPersonality : Personality {
-        public abstract float[] vec { get; }
-
         public abstract void calculate(TPersonality ply);
     }
 }
