a7bc77ccd2beab176a8a71f7c011bf94eab1d3e1
xdrie <xdrie@alt.icu>
2020-09-07 20:29:28 -0700

    add support for raw vector traits

--- src/DuckMind/Ducia/Cogs/Personality.cs
+++ src/DuckMind/Ducia/Cogs/Personality.cs
@@ -5,9 +5,23 @@ namespace Ducia.Cogs {
     public abstract class Personality {
         public abstract float[] vec { get; }
 
+        /// <summary>
+        /// dot product of normalized vec and weights
+        /// </summary>
+        /// <param name="weights"></param>
+        /// <returns></returns>
         public float mult(float[] weights) {
             var nvec = GMathf.normVec(vec); // normalized personality vector
             return GMathf.dot(nvec, weights);
+        }
+
+        /// <summary>
+        /// dot product of raw vec and weights, without normalization
+        /// </summary>
+        /// <param name="weights"></param>
+        /// <returns></returns>
+        public float multRaw(float[] weights) {
+            return GMathf.dot(vec, weights);
         }
 
         public static float normalRand(float u, float s) {
--- src/DuckMind/Ducia/Cogs/VectorTrait.cs
+++ src/DuckMind/Ducia/Cogs/VectorTrait.cs
@@ -4,14 +4,26 @@ namespace Ducia.Cogs {
     public static class VectorTrait {
         /// <summary>
         /// calculate the value of a trait for a personality
+        /// normalizes both the weight vector and the personality vector
         /// </summary>
         /// <param name="weights">trait weights</param>
         /// <param name="p">personality</param>
         /// <returns></returns>
-        public static float value(float[] weights, Personality p) {
-            var nweights = GMathf.normVec(weights); // normalized weight vec
-            var result = p.mult(nweights);
+        public static float normalizedValue(float[] weights, Personality p) {
+            var normalizedWeights = GMathf.normVec(weights); // normalized weight vec
+            var result = p.mult(normalizedWeights);
             return result;
+        }
+
+        /// <summary>
+        /// computes the raw dot product of the weight vector and the personality vector
+        /// without normalization
+        /// </summary>
+        /// <param name="weights"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static float rawValue(float[] weights, Personality p) {
+            return p.multRaw(weights);
         }
 
         /// <summary>
