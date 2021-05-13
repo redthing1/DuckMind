d119874ce80fbde280032bc922d2c0ed2504a49c
xdrie <xhyinne@gmail.com>
2021-05-12 18:33:48 -0700

    make comparison more explicit

--- src/DuckMind/Ducia.Tests/Cogs/PersonalityTests.cs
+++ src/DuckMind/Ducia.Tests/Cogs/PersonalityTests.cs
@@ -15,7 +15,7 @@ namespace Ducia.Tests.Cogs {
             var v01 = PerMath.map01(vi);
             Assert.Equal(0.5f, v01, 4);
             var v11 = PerMath.map11(v01);
-            Assert.Equal(vi, v11, 4);
+            Assert.Equal(0f, v11, 4);
         }
 
         [Fact]
