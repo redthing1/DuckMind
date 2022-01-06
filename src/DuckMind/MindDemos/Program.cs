47c6cb26ad7e21cd5c01a706078c57bb80d9a2a1
redthing1 <xhyinne@gmail.com>
2022-01-06 15:34:04 -0800

    add logger to minddemos

--- src/DuckMind/MindDemos/Program.cs
+++ src/DuckMind/MindDemos/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using Ducia.Tests.Framework.Utility;
+using Iri.Glass.Logging;
 using Xunit;
 
 namespace MindDemos {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("mind demos");
-            
+
+            var log = new Logger(Logger.Verbosity.Information);
+            log.verbosity = Logger.Verbosity.Trace;
+            log.sinks.Add(new Logger.ConsoleSink());
+
+            log.info("testing cake game");
             // test cake game
             var game = new CakeGame();
             var result = game.run(100000);
             Assert.True(result);
+            log.info("cake game success");
         }
     }
 }
